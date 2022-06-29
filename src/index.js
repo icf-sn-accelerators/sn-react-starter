40f00bedf56ecd90a96092f23ef583a877f6b01d
John MacKenzie <john.mackenzie@icf.com>
2022-06-29 14:03:47 -0400

    updating out of date dependencies / updating react

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { HashRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import './styles/index.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
